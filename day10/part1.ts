cc4195c866d4eebd78d9cf874d6b0e1d8eef4780
Mario Lubenka <me@saitho.me>
2023-12-11 08:20:21 +0100

    day10 part1 beta

--- day10/part1.ts
+++ day10/part1.ts
@@ -24,24 +24,43 @@ function determineJunctionType(coords: Coord) {
         top: map.getChar(coords.x, coords.y-1),
         bottom: map.getChar(coords.x, coords.y+1),
     }
+    const possibleJunctions = []
     if (!invalidPoints.includes(adjacentPoints.top) && !invalidPoints.includes(adjacentPoints.bottom)) {
-        return '|'
-    } else if (!invalidPoints.includes(adjacentPoints.left) && !invalidPoints.includes(adjacentPoints.right)) {
-        return '-'
-    } else if (!invalidPoints.includes(adjacentPoints.top) && !invalidPoints.includes(adjacentPoints.right)) {
-        return 'L'
-    } else if (!invalidPoints.includes(adjacentPoints.bottom) && !invalidPoints.includes(adjacentPoints.right)) {
-        return 'F'
-    } else if (!invalidPoints.includes(adjacentPoints.top) && !invalidPoints.includes(adjacentPoints.left)) {
-        return 'J'
-    } else if (!invalidPoints.includes(adjacentPoints.bottom) && !invalidPoints.includes(adjacentPoints.left)) {
-        return '7'
+        possibleJunctions.push('|')
     }
-    return ''
+    if (!invalidPoints.includes(adjacentPoints.left) && !invalidPoints.includes(adjacentPoints.right)) {
+        possibleJunctions.push('-')
+    }
+    if (!invalidPoints.includes(adjacentPoints.top) && !invalidPoints.includes(adjacentPoints.right)) {
+        possibleJunctions.push('L')
+    }
+    if (!invalidPoints.includes(adjacentPoints.bottom) && !invalidPoints.includes(adjacentPoints.right)) {
+        possibleJunctions.push('F')
+    }
+    if (!invalidPoints.includes(adjacentPoints.top) && !invalidPoints.includes(adjacentPoints.left)) {
+        possibleJunctions.push('J')
+    }
+    if (!invalidPoints.includes(adjacentPoints.bottom) && !invalidPoints.includes(adjacentPoints.left)) {
+        possibleJunctions.push('7')
+    }
+
+    for (const junction of possibleJunctions) {
+        console.log('junction')
+    }
+
+    if (possibleJunctions.length > 1) {
+        throw Error('more than one junction found')
+    }
+    return possibleJunctions.shift()
 }
 
 function isValidTransition(from: string, to: string): boolean
 {
+    if (to === undefined) {
+        return false
+    }
+    return to !== '.'
+
     if (from === '-' || to === '-') {
         return true
     }
@@ -72,7 +91,7 @@ function getPossibleCoords(coords: Coord) {
         return []
     }
     if (!validJunctionTypes.includes(currentChar)) {
-        const junctionType = determineJunctionType(coords)
+        const junctionType = determineJunctionType(coords)!
         console.log('Replace char "' + currentChar + '" at (' + coords.x + ',' + coords.y + ') with "' + junctionType + '"')
         currentChar = junctionType
     }
@@ -95,18 +114,18 @@ function getPossibleCoords(coords: Coord) {
     } else if (currentChar === 'L') {
         // top
         possibleCoords.push({x: coords.x, y: coords.y-1})
-        // left
-        possibleCoords.push({x: coords.x+1, y: coords.y})
-    } else if (currentChar === '7') {
-        // bottom
-        possibleCoords.push({x: coords.x, y: coords.y+1})
-        // left
-        possibleCoords.push({x: coords.x+1, y: coords.y})
-    } else if (currentChar === 'F') {
-        // bottom
-        possibleCoords.push({x: coords.x, y: coords.y+1})
         // right
         possibleCoords.push({x: coords.x+1, y: coords.y})
+    } else if (currentChar === '7') {
+        // left
+        possibleCoords.push({x: coords.x+1, y: coords.y})
+        // bottom
+        possibleCoords.push({x: coords.x, y: coords.y+1})
+    } else if (currentChar === 'F') {
+        // right
+        possibleCoords.push({x: coords.x+1, y: coords.y})
+        // bottom
+        possibleCoords.push({x: coords.x, y: coords.y+1})
     }
     return possibleCoords
 }
@@ -120,32 +139,57 @@ function getPossibleCoords(coords: Coord) {
 // . is ground; there is no pipe in this tile.
 // S is the starting position of the animal; there is a pipe on this tile, but your sketch doesn't show what shape the pipe has.
 let traverseLog = new Map<number, any[]>()
-function traverse(coords: Coord, distance: number, visitedLocations: Coord[]) {
+
+let visitedLocations: Coord[] = []
+function traverse(coords: Coord, distance: number): Coord[] {
     if (distance > 0 && (coords.x === startCoord.x && coords.y === startCoord.y)) {
-        return
+        return []
     }
-    // .includes() does not work on objects
-    if (visitedLocations.filter((v) => v.x === coords.x && v.y === coords.y).length) {
-        return
-    }
-    if (!traverseLog.has(distance+1)) {
-        traverseLog.set(distance+1, [])
-    }
+    //console.log('visiting ' + coords.x + ',' + coords.y)
+    distance++
     const possibleCoords = getPossibleCoords(coords)
     visitedLocations.push(coords)
     let currentChar = map.getChar(coords.x, coords.y)!
 
+    const nextTraversalCoords = []
     for (const coord of possibleCoords) {
         const char = map.getChar(coord.x, coord.y)!
+        if (visitedLocations.filter((v) => v.x === coord.x && v.y === coord.y).length) {
+            continue
+        }
+        if (char === '.') {
+            continue
+        }
+        if (char === undefined) {
+            continue
+        }
         if (!isValidTransition(currentChar, char)) {
+            console.log('invalid transition "' + currentChar + '" -> "' + char + '"')
             continue
         }
-        if (char === startPoint || char === undefined || char === '.') {
+        if (char === startPoint) {
             continue
         }
-        traverseLog.get(distance+1)!.push(char)
-        traverse(coord, distance+1, visitedLocations)
+        //console.log(`check transition "${currentChar}" -> "${char}" ([${coords.x},${coords.y}] -> [${coord.x},${coord.y}])`)
+        if (!traverseLog.has(distance)) {
+            traverseLog.set(distance, [])
+        }
+        traverseLog.get(distance)!.push({char, coord})
+        nextTraversalCoords.push(coord)
     }
+    return nextTraversalCoords
 }
 
-traverse(startCoord, 0, [])
+let nextTraversalCoords = [startCoord]
+let distance = 0
+do {
+    let nextCoords = []
+    for (let coord of nextTraversalCoords) {
+        nextCoords.push(...traverse(coord, distance))
+    }
+    nextTraversalCoords = nextCoords
+    distance++
+} while (nextTraversalCoords.length)
+
+// 159 too low
+console.log(traverseLog.size)
